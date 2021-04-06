5f1f7933c87b7e384b2132750c93bcbec7f2b6b3
Abdul Rahman Saleh <abdulrahmansaleh152@gmail.com>
2021-04-06 11:16:11 +0700

    membuat file untuk List of Color

---
+++ lib/widgets/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+List<Color> categoryColors = [
+  Colors.red[400],
+  Colors.orange[400],
+  Colors.green[400],
+  Colors.blue[400],
+  Colors.purple[400],
+];
+
+List<Color> memoColors = [
+  Colors.red[400],
+  Colors.orange[400],
+  Colors.green[400],
+  Colors.blue[400],
+  Colors.purple[400],
+  Colors.pink[400],
+  Colors.yellow[400],
+  Colors.brown[400],
+  Colors.teal[400],
+  Colors.indigo[400],
+];
