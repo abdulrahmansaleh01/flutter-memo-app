74934ba72420ff6cdecd182e851b951d94385d4e
Abdul Rahman Saleh <abdulrahmansaleh152@gmail.com>
2021-04-15 12:09:03 +0700

    update list of color

--- lib/widgets/colors.dart
+++ lib/widgets/colors.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 
 List<Color> categoryColors = [
-  Colors.red[400],
-  Colors.orange[400],
-  Colors.green[400],
-  Colors.blue[400],
-  Colors.purple[400],
+  Colors.red[700],
+  Colors.orange[700],
+  Colors.green[700],
+  Colors.blue[700],
+  Colors.purple[700],
 ];
 
 List<Color> memoColors = [
-  Colors.red[400],
-  Colors.orange[400],
-  Colors.green[400],
-  Colors.blue[400],
-  Colors.purple[400],
-  Colors.pink[400],
-  Colors.yellow[400],
-  Colors.brown[400],
-  Colors.teal[400],
-  Colors.indigo[400],
+  Colors.red[700],
+  Colors.orange[700],
+  Colors.green[700],
+  Colors.blue[700],
+  Colors.purple[700],
+  Colors.pink[700],
+  Colors.yellow[700],
+  Colors.brown[700],
+  Colors.teal[700],
+  Colors.indigo[700],
 ];
